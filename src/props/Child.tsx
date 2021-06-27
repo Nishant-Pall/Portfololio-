739a9176b949e8bde6961db5b15347bc78f9062c
Nishant-Pall <palln6935@gmail.com>
2021-06-28 05:22:39 +0530

    Add interface for child component

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,3 +1,12 @@
-export const Child = ()=>{
-	return <div>Hello World</div>
+interface ChildProps{
+	color:string;
+}
+
+// Interfaces provide security checks, first is to ensure whether
+// parent component is sending in props, and second is to verify
+// that the type of the variable is as according to the interface
+// object
+
+export const Child = ({color} : ChildProps)=>{
+	return <div>{color}</div>
 }
