2b4661494b9ae6c66c32e624dc7e274fee26b53e
Nishant-Pall <palln6935@gmail.com>
2021-06-29 03:19:02 +0530

    Add annotations with children

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,5 +1,6 @@
 interface ChildProps{
 	color:string;
+    onClick: () => void;
 }
 
 // Interfaces provide security checks, first is to ensure whether
@@ -7,6 +8,27 @@ interface ChildProps{
 // that the type of the variable is as according to the interface
 // object
 
-export const Child = ({color} : ChildProps)=>{
-	return <div>{color}</div>
+// Downside: TS doesn't understand that we're defining a
+// REACT component, so it thanks that Child doesn't have properties
+// like propTypes, displayName, defaultProps, contextTypes
+// which are optional properties provided by react components
+export const Child = ({color, onClick} : ChildProps)=>{
+	return <div>
+    {color}
+    <button onClick={onClick}>Click me</button>
+    </div>
+}
+
+// React.FunctionComponent
+// Another way of writing a react component, this
+// let's TS know we're exporting a react component
+//
+// Another upside is that we don't need to specify the children prop
+// from parent component inside an interface
+export const ChildAsFc: React.FC<ChildProps> = ({color, onClick, children}) =>{
+	return <div>
+    {color}
+    {children}
+    <button onClick={onClick}>Click Me</button>
+    </div>
 }
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,14 @@
-import {Child} from './Child'
+import {ChildAsFc} from './Child'
 
+// If we use opening and closing tags in a react component
+// the content inside is automatically passed as the children
+// prop, this will give error if we did this in Child component
+// because the interface doesn't know about the children prop
+// whereas ChildAsFc, knows because we're using React.FunctionComponent
 const Parent= ()=>{
-	return <Child color="red"/>
+	return <ChildAsFc color="red" onClick={()=>console.log("Clicked")}>
+            This is default children prop
+        </ChildAsFc>
 }
 
 export default Parent
