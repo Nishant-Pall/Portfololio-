89ff3cc082985f963c3be0e9802c638f67cd72f0
Nishant-Pall <palln6935@gmail.com>
2021-06-30 00:24:30 +0530

    Complete guestList

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -12,6 +12,9 @@ const GuestList: React.FC = () => {
   return (
     <div>
       <h3>Guest list</h3>
+      <ul>
+        {guests.map((guest) => <li key={guest}>{guest}</li>)}
+      </ul>
       <input value={name} onChange={(e) => setName(e.target.value)} />
       <button onClick={onClick}>Add guest</button>
     </div>
